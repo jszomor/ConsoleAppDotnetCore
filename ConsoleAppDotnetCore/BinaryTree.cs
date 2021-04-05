ad63f1f1413cc039759f0477df3283fcc18aa23e
Szomor János <jszomor@gmail.com>
2021-04-05 09:55:45 +0200

    BinaryTree

---
+++ ConsoleAppDotnetCore/BinaryTree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppDotnetCore
+{
+  public class TreeNode
+  {
+    public int Value { get; set; }
+    public TreeNode(int value)
+    {
+      Value = value;
+    }
+
+    public TreeNode Parent { get; set; }
+    public TreeNode Left { get; set; }
+    public TreeNode Right { get; set; }
+  }
+  public class BinaryTree
+  {
+    public BinaryTree() { }
+    public BinaryTree(int[] values)
+    {
+      foreach (var value in values)
+      {
+        this.Insert(value);
+      }
+    }
+
+    public TreeNode Root { get; set; }
+
+    private void Insert(int value) 
+    {
+      if (Root == null)
+      {
+        Root = new TreeNode(value);
+      }
+      else
+      {
+        TreeNode current = Root;
+
+        while(current != null)
+        {
+          if (current.Value > value)
+          {
+            if (current.Left == null)
+            {
+              current.Left = new TreeNode(value);
+              current.Left.Parent = current;
+              return;
+            }
+            else
+            {
+              current = current.Left;
+            }
+          }
+          else if (current.Value < value)
+          {
+            if (current.Right == null)
+            {
+              current.Right = new TreeNode(value);
+              current.Right.Parent = current;
+              return;
+            }
+            else
+            {
+              current = current.Right;
+            }
+          }
+          else return;
+        }
+      }
+    }
+
+    public void InOrder(Action<int> action)
+    {
+      _inOrder(Root, action);
+    }
+
+    private void _inOrder(TreeNode root, Action<int> action)
+    {
+      if(root == null) { return; }
+
+      _inOrder(root.Left, action);
+      action(root.Value);
+      _inOrder(root.Right, action);
+    }
+  }
+}
--- ConsoleAppDotnetCore/Program.cs
+++ ConsoleAppDotnetCore/Program.cs
@@ -12,7 +12,10 @@ namespace ConsoleAppDotnetCore
 
       //Occurrences.CountWordDict();
 
-      //int[] numbers = new int[] { 3, 9, 10, 6, 8, 11 };
+      int[] numbers = new int[] { 10, 1, 4, 6, 6, 3, 9, 12 };
+
+      var binaryTree = new BinaryTree(numbers);
+
 
       //var quickSort = new MyArray(numbers);
 
@@ -23,33 +26,33 @@ namespace ConsoleAppDotnetCore
       //  Console.WriteLine(item);
       //}
 
-      while(true)
-      {
-        Console.WriteLine("Enter the first word.");
-        string word1 = Console.ReadLine();
-        Console.WriteLine("Enter the second word.");
-        string word2 = Console.ReadLine();
+      //while(true)
+      //{
+      //  Console.WriteLine("Enter the first word.");
+      //  string word1 = Console.ReadLine();
+      //  Console.WriteLine("Enter the second word.");
+      //  string word2 = Console.ReadLine();
 
-        Words words = new Words(word1, word2);
-        var anag = new Anagram();
-        anag.CompareAnagram(words);
-        string answer;
-        while (true)
-        {
-          Console.WriteLine("Wants to try again?(Y/N)");
-          answer = Console.ReadLine();
+      //  Words words = new Words(word1, word2);
+      //  var anag = new Anagram();
+      //  anag.CompareAnagram(words);
+      //  string answer;
+      //  while (true)
+      //  {
+      //    Console.WriteLine("Wants to try again?(Y/N)");
+      //    answer = Console.ReadLine();
 
-          if (answer != "Y" && answer != "N")
-          {
-            Console.WriteLine("Invalid answer. Please select letter [Y] for keep continue or letter [N] for exit");
-          }
-          else
-            break;
-        }
+      //    if (answer != "Y" && answer != "N")
+      //    {
+      //      Console.WriteLine("Invalid answer. Please select letter [Y] for keep continue or letter [N] for exit");
+      //    }
+      //    else
+      //      break;
+      //  }
 
-        if (answer == "N")
-          break;
-      }
+      //  if (answer == "N")
+      //    break;
+      //}
 
       //Occurrences.WordCountLINQ2();
 
