aabb4d5851ba9c82d687d466b399d4bbfdb7bc13
Szomor János <jszomor@gmail.com>
2021-06-24 20:09:48 +0200

    SampleMethods added

--- SoYouThinkYouCanLINQ/Program.cs
+++ SoYouThinkYouCanLINQ/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace SoYouThinkYouCanLINQ
 {
@@ -8,36 +9,79 @@ namespace SoYouThinkYouCanLINQ
     {
       #region President section
 
-      var preses = new Presidents();
+      //var preses = new Presidents();
 
 
-      foreach (var item in preses)
+      //foreach (var item in preses)
+      //{
+      //  Console.WriteLine(item);
+
+      //}
+
+      ////behind the scene the complire rewrite the foreach loop similar to the below code
+      //using (var enumerator = preses.GetEnumerator())
+      //{
+      //  while (enumerator.MoveNext())
+      //    Console.WriteLine(enumerator.Current);
+      //}
+
+      //var alive = from p in preses
+      //            where p.IsAlive // Manual implementation of where function from LINQ
+      //            select p;
+
+      //var aliveWithoutLINQSyntax = preses.Where(preses.InstedOfLINQSyntax);
+
+
+      //Console.WriteLine();
+
+      //foreach (var item in alive)
+      //{
+      //  Console.WriteLine(item);
+      //}
+
+
+      //Console.ReadKey();
+
+      #endregion
+
+      #region Anonymus delegates
+
+      int[] num = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+      var result = num.Where(SampleMethods.MyPredicate);
+
+      var result2 = num.Where(n => n % 2 == 0);
+
+      var result3 = num.Where(delegate(int num)
+      {
+        return num % 2 == 0;
+      });
+
+      Func<int, bool> myDel = delegate (int num)
+      {
+        return num % 2 == 0;
+      };
+
+      var result4 = num.Where(myDel);
+
+
+      foreach (var item in result4)
       {
         Console.WriteLine(item);
-
       }
 
-      //behind the scene the complire rewrite the foreach loop similar to the below code
-      using (var enumerator = preses.GetEnumerator())
-      {
-        while (enumerator.MoveNext())
-          Console.WriteLine(enumerator.Current);
-      }
 
-      var alive = from p in preses
-                  where p.IsAlive // Manual implementation of where function from LINQ
-                  select p;
+      //TransformDelegate a, b, c, d;
 
-      var aliveWithoutLINQSyntax = preses.Where(preses.InstedOfLINQSyntax);
+      //a = SamleMethods.Method1;
 
+      //b = SamleMethods.Method2;
 
-      Console.WriteLine();
+      //c = a + b; //delegates have multicast feature
 
-      foreach (var item in alive)
-      {
-        Console.WriteLine(item);
-      }
+      //d = c - a;
 
+      //d("b");
 
       Console.ReadKey();
 
---
+++ SoYouThinkYouCanLINQ/SampleMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoYouThinkYouCanLINQ
+{
+
+  public delegate void TransformDelegate(string num);
+
+  public class SampleMethods
+  {
+    public static bool MyPredicate(int num)
+    {
+      return num % 2 == 0;
+    }
+
+    public static void Method1(string data)
+    {
+      Console.WriteLine(data + ": Method 1");
+    }
+
+    public static void Method2(string data)
+    {
+      Console.WriteLine(data + ": Method 2");
+    }
+  }
+
+
+}
