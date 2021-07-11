b09f7f29954df2a8ad9c05b6676071242b8ce8a7
Szomor János <jszomor@gmail.com>
2021-07-11 16:49:02 +0200

    MinimumAbsoluteDifference

---
+++ Hackerrank/GreedyAlgorithm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerrank
+{
+  class GreedyAlgorithm
+  {
+    public void MinimumAbsoluteDifference()
+    {
+      List<int> arr = new List<int>{ -59, -36, -13, 1, -53, -92, -2, -96, -54, 75 };
+
+      long min = 100000000000000;
+      int value;
+      arr.Sort();
+
+      #region Multiple loop
+      //for (int i = 0; i < arr.Count; i++)
+      //{
+      //  for (int j = arr.Count-1; j > i; j--)
+      //  {
+      //    value = arr[i] - arr[j];
+      //    value = Math.Abs(value);
+      //    Console.WriteLine($"{value}, i:{i}, j:{j}");
+
+      //    if(value < min)
+      //    {
+      //      min = value;
+      //    }
+      //  }
+      //  Console.WriteLine("------------");
+      //}
+      //Console.WriteLine(min);
+      #endregion
+
+      for (int i = 1; i < arr.Count; i++)
+      {
+        value = arr[i - 1] - arr[i];
+        value = Math.Abs(value);
+
+        if(value < min)
+        {
+          min = value;
+        }
+      }
+      Console.WriteLine(min);
+    }
+  }
+}
---
+++ Hackerrank/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hackerrank
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var greedy = new GreedyAlgorithm();
+
+      greedy.MinimumAbsoluteDifference();
+    }
+  }
+}
